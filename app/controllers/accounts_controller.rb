f107606ae2dc093c6f616d891a860e96b9ed1134
CodyDudik <Cody.dudik@gmail.com>
2025-09-16 15:43:21 -0700

    Added folders for devise to make all the password pages viewable and match our site theme for sign in/out/reset, with controllers and layout too. Added views for file record new/show/edit, home pages, account edit/viewing. Fix banner a bit to be the same on all pages. Moved sign in/out to middle page, instead of in the banner.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -6,5 +6,17 @@ class AccountsController < ApplicationController
   end
 
   def update
+    if current_user.update(account_params)
+      redirect_to account_path, notice: "Account updated successfully."
+    else
+      flash.now[:alert] = "Failed to update account."
+      render :edit
+    end
+  end
+
+  private
+
+  def account_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
